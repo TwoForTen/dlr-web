51fdd332eadf27cd90cc696a852271b7c73208d3
NoelRostohar <noel.rostohar@arsfutura.co>
2021-09-25 20:33:48 +0200

    WIP Placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,34 @@
-import Head from 'next/head'
-
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<div className='h-screen w-screen bg-site-offline relative flex justify-center items-center'>
+			<div className='absolute top-0 left-0 bg-primary h-screen w-screen opacity-90 z-0' />
+			<div className='z-10 flex flex-col sm:flex-row items-center justify-center'>
+				<div className='flex flex-col z-10 mb-16 sm:mb-0 sm:mr-24 items-center'>
+					<img src='/icons/dlr_logo.svg' className='h-48' />
+					<h1 className='text-2xl text-white mb-4'>Stranica u izradi...</h1>
+					<img src='/icons/loading.svg' className='h-12' />
+				</div>
+				<div className='flex flex-col z-10 text-center sm:text-left'>
+					<div className='mb-6'>
+						<span className='text-white opacity-60'>Email</span>
+						<h2 className='text-white font-medium text-xl'>
+							denis.rostohar@gmail.com
+						</h2>
+					</div>
+					<div className='mb-6'>
+						<span className='text-white opacity-60'>Telefon</span>
+						<h2 className='text-white font-medium text-xl'>+385 91 949 0892</h2>
+					</div>
+					<div>
+						<span className='text-white opacity-60'>Adresa</span>
+						<h2 className='text-white font-medium text-xl'>
+							Draganička grana 17,
+							<br />
+							<span className='text-white font-medium'>10 000 Zagreb</span>
+						</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: "jit",
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false,
+	theme: {
+		extend: {
+			colors: {
+				primary: "#24336A",
+				label: "#6E84D2",
+			},
+			backgroundImage: {
+				"site-offline": "url('/images/site_offline.jpg')",
+			},
+		},
+	},
+};
